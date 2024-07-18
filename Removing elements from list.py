be5ef1b7c233e9f0a234f6f1ba836bf6658ee1ed
Anurag Singh Yadav <117581504+AnuragSinghYadav@users.noreply.github.com>
2024-07-18 13:28:57 +0530

    Add files via upload

---
+++ Removing elements from list.py
@@ -0,0 +1,9 @@
+#remove() remove specified item
+#pop() remove items at given index or else last items list.pop()in case in last item
+# fruits = ["apple", "banana","Cherry"]
+# fruits.remove("banana")
+# print(fruits)
+
+fruits = ["apple", "banana","Cherry"]
+fruits.pop(1)
+print(fruits)
